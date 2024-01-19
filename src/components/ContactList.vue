d188e4398d68056f27929a4fd3ee1e809a69038e
Olivier Delmas <olivier.delmas@edu.devinci.fr>
2024-01-19 10:49:12 +0100

    :fire: affichage contact avec component + store associé

---
+++ src/components/ContactList.vue
@@ -0,0 +1,18 @@
+<script>
+    export default {
+      props: {
+        contacts: {},
+      },
+    };
+  </script>
+
+<template>
+    <div>
+      <h2>Contact List</h2>
+      <ul>
+        <li v-for="contact in contacts" :key="contact.name">
+          {{ contact.name }} - {{ contact.phoneNumber }}
+        </li>
+      </ul>
+    </div>
+  </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,23 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+<!-- views/HomeView.vue -->
+<script>
+  import { useContactStore } from '@/stores/contact';
+  import ContactList from '@/components/ContactList.vue';
+
+  export default {
+    name: 'HomeView',
+    components: {
+      ContactList,
+    },
+    setup() {
+      const contacts  = useContactStore().contacts;
+      return { contacts };
+    },
+  };
 </script>
 
 <template>
   <main>
     <TheWelcome />
+    <ContactList :contacts="contacts" />
   </main>
 </template>
